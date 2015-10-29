6e4d3ab24f06986ca311717ea311595610efcd35
Todd Courtnage <todd@chaordix.com>
2015-10-29 16:28:44 -0600

    I don't want puppet to manage the client.keys file right now.

--- manifests/server.pp
+++ manifests/server.pp
@@ -79,19 +79,19 @@ class ossec::server (
     notify  => Service[$ossec::common::hidsserverservice]
   }
 
-  concat { '/var/ossec/etc/client.keys':
-    owner   => 'root',
-    group   => 'ossec',
-    mode    => '0640',
-    notify  => Service[$ossec::common::hidsserverservice],
-    require => Package[$ossec::common::hidsserverpackage],
-  }
-  concat::fragment { 'var_ossec_etc_client.keys_end' :
-    target  => '/var/ossec/etc/client.keys',
-    order   => 99,
-    content => "\n",
-    notify  => Service[$ossec::common::hidsserverservice]
-  }
-  Ossec::Agentkey<<| |>>
+  #concat { '/var/ossec/etc/client.keys':
+  #  owner   => 'root',
+  #  group   => 'ossec',
+  #  mode    => '0640',
+  #  notify  => Service[$ossec::common::hidsserverservice],
+  #  require => Package[$ossec::common::hidsserverpackage],
+  #}
+  #concat::fragment { 'var_ossec_etc_client.keys_end' :
+  #  target  => '/var/ossec/etc/client.keys',
+  #  order   => 99,
+  #  content => "\n",
+  #  notify  => Service[$ossec::common::hidsserverservice]
+  #}
+  #Ossec::Agentkey<<| |>>
 
 }
